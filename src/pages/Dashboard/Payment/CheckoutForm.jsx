f4a7a6e84d312955b60ae0d5654ea19acac1ecc7
fd-Martin <fdmosharof@gmail.com>
2024-05-16 12:17:05 +0600

    Use Test Card And Display Card Error

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,23 +1,39 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const CheckoutForm = () => {
     const stripe = useStripe();
-    const elements =useElements();
+    const elements = useElements();
+    const [cardError,setCardError]=useState('');
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return
         }
 
-        const card =elements.getElement(CardElement);
-        if(card===null){
+        const card = elements.getElement(CardElement);
+        if (card === null) {
             return
+        }
+        console.log('card', card);
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+
+        if (error) {
+            console.log('error', error);
+            setCardError(error.message)
+        } else {
+            console.log('PaymentMethod', paymentMethod);
+            setCardError('')
         }
     }
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            <form className='w-2/3 m-8' onSubmit={handleSubmit}>
                 <CardElement
                     options={{
                         style: {
@@ -34,10 +50,11 @@ const CheckoutForm = () => {
                         },
                     }}
                 />
-                <button type="submit" disabled={!stripe}>
+                <button type="submit" className='btn  btn-primary btn-sm mt-4' disabled={!stripe}>
                     Pay
                 </button>
             </form>
+            {cardError&& <p className='text-red-600 mt-8'>{cardError}</p> }
         </>
     );
 };

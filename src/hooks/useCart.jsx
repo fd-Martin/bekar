ea85161ba934abba923c5428c5260ef2e0ff40fc
fd-Martin <fdmosharof@gmail.com>
2024-05-16 17:00:22 +0600

    Get Price For Payment Intent And Understand Array Reduce

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -9,14 +9,7 @@ const useCart = () => {
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         enabled:!loading,
-        // queryFn: async () => {
-        //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
-        //         headers:{
-        //             authorization: `bearer ${token}`
-        //         }
-        //     }) 
-        //     return res.json();
-        // },
+    
  
         queryFn: async () => {
             const res = await axiosSecure(`/carts?email=${user?.email}`)
@@ -29,3 +22,12 @@ const useCart = () => {
 
 }
 export default useCart;
+
+    // queryFn: async () => {
+        //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
+        //         headers:{
+        //             authorization: `bearer ${token}`
+        //         }
+        //     }) 
+        //     return res.json();
+        // },
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -3,15 +3,19 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import CheckoutForm from './CheckoutForm';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import useCart from '../../../hooks/useCart';
 //todo : provide publishable key
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
+    const [cart]=useCart();
+    const total= cart.reduce((sum,item)=>sum+item.price,0);
+    const price=parseFloat(total.toFixed(2));
     return (
         <div>
             <SectionTitle subHeading='Please Process' heading='Payment'></SectionTitle>
             <h2 className='text-3xl'>Payment </h2>
             <Elements stripe={stripePromise}>
-                <CheckoutForm></CheckoutForm>
+                <CheckoutForm price={price} ></CheckoutForm>
             </Elements>
         </div>
     );
